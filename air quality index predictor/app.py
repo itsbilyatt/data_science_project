99124d23b9fbed863879bd8d2d703952a17254fa
itsbilyat <106489353+itsbilyatt@users.noreply.github.com>
2023-09-10 15:48:02 +0530

    Update app.py

--- air quality index predictor/app.py
+++ air quality index predictor/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
+# prajyot birajdar
+# prajyotbirajada1998@gmail.com
 
 @app.route('/', methods=['POST'])
 def home_post():
